0bfe5acd5260129b6162e279edbd0bf6741a0ce8
Omar <oai4@njit.edu>
2020-10-28 14:51:29 -0400

    Add Post fully functional. Home page tweaks.

--- blog.js
+++ blog.js
@@ -1,3 +1,14 @@
+//Internal
+function getCurrentDate(){
+    const month = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+var date = new Date();
+var dd = String(date.getDate()).padStart(2, '0');
+return month[date.getMonth()]+" "+dd+", "+date.getFullYear();
+}
+
 //Div functions
 function showAddDiv() {
     //Check if already up
@@ -20,6 +31,22 @@ function showAddDiv() {
 }
 
 function hideAddDiv() {
+    //Display confirmation (only if not blank)
+    var addArticleDiv = document.getElementById("addArticleDiv");
+    var title = document.getElementById("addTitle");
+    var author = document.getElementById("addAuthor");
+    var article = document.getElementById("addArticle");
+    
+    //Discard and hide
+    addArticleDiv.style.display = "none";
+
+    //Clear all text that is in post
+    title.value = "";
+    author.value = "";
+    article.value = "";
+}
+
+function hideAddDivConfirm() {
     //Display confirmation (only if not blank)
     var addArticleDiv = document.getElementById("addArticleDiv");
     var title = document.getElementById("addTitle");
@@ -57,3 +84,44 @@ function validateAddPost() {
     }
 }
 
+//Publish
+function publishArticle(){
+    var title = document.getElementById("addTitle");
+    var author = document.getElementById("addAuthor");
+    var article = document.getElementById("addArticle");
+
+    var mainContent=document.getElementById("mainContent");
+
+    //Create blog post
+    var div=document.createElement("div");
+    div.className="blog-post";
+
+    var h2=document.createElement("h2");
+    h2.className="blog-post-title";
+    h2.innerHTML=title.value;
+    div.appendChild(h2);
+
+    var p1=document.createElement("p");
+    p1.className="blog-post-meta";
+    p1.innerHTML=getCurrentDate()+" by "; //Get Date
+    var authorRef=document.createElement("a"); //Set author as link
+    authorRef.href='#';
+    authorRef.innerHTML=author.value;
+    p1.innerHTML+=authorRef.innerHTML;
+    div.appendChild(p1);
+
+    var p2=document.createElement("p");
+    p2.innerHTML=article.value;
+    div.appendChild(p2);
+
+    var hr=document.createElement("hr");
+    div.appendChild(hr);
+
+    //Give confirmation
+    alert("Blog Post Published Successfully!");
+
+    //Publish and hide Add Post
+    document.getElementById("yourPosts").style.display="block";
+    mainContent.prepend(div); //Adds to beginning rather than end
+    hideAddDiv();
+}
