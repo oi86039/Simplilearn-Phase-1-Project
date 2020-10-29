96cb063855eb313c2fccd51cec3288b8a298842d
Omar <oai4@njit.edu>
2020-10-29 13:14:08 -0400

    Contact Page nearly finished. Needs polish

---
+++ contact.js
@@ -0,0 +1,8 @@
+function submitContactForm(){
+    alert("sup");
+    document.getElementById("contact").submit();
+}
+
+function confirmSubmit(){
+    alert("Message sent! (Note: not really, this is fake.");
+}
